0d9b3691792fbd3088066b907c6b3bd3ac26f4f1
brandonpittman <brandonpittman@gmail.com>
2013-09-24 00:16:56 +0900

    Update RunAnyScript.rb
    Add comment to line 3

--- ShellScripts/RunAnyScript.rb
+++ ShellScripts/RunAnyScript.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-f = ARGF.argv[0]
+f = ARGF.argv[0] # the idea is, you pass Otto's Remote a script file you have on your Mac at home
 ext = File.extname(f)
 
 prepend = 'ruby ' if ext == '.rb'
